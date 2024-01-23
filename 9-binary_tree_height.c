3927c53ee200b1fa36dc692a22d9c22546fa362c
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-01-23 08:03:45 -0800

    Betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - Calculates the height of a binary tree.
+ *
+ * Computes the height of the binary tree, which is the length of the
+ * longest path from the root node to a leaf node.
+ *
+ * @tree: Pointer to the root of the binary tree.
+ *
+ * Return: Height of the binary tree, or 0 if the tree is NULL.
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_side, right_side;
@@ -11,7 +21,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	right_side = binary_tree_height(tree->right);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return(0);
+		return (0);
 
 	if (left_side < right_side)
 		return (left_side + 1);
