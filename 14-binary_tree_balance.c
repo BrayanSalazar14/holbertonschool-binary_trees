b46381eeeb3178c07f6265bf9771e5dc6b145e01
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-01-24 07:18:21 -0800

    VF

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,9 +18,28 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if ((!tree->left && tree->right) || (tree->left && !tree->right))
-		return (1);
+		return (-1);
 
-	left = binary_tree_balance(tree->left);
-	right = binary_tree_balance(tree->right);
-	return (left + right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
+}
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t left_side, right_side;
+
+        if (tree == NULL)
+                return (0);
+
+        left_side = binary_tree_height(tree->left);
+        right_side = binary_tree_height(tree->right);
+
+        if (tree->left == NULL && tree->right == NULL)
+                return (0);
+
+        if (left_side > right_side)
+                return (left_side + 1);
+        else
+                return (right_side + 1);
 }
