b3146bda317db87b6f117f187521ce3a254bea8f
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-01-25 07:54:12 -0800

    V5, change for code and implementation 2da function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,16 +18,43 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (!tree->left && !tree->right)
-		return (0);
+		return (1);
 
-	left = binary_tree_is_perfect(tree->left);
-	right = binary_tree_is_perfect(tree->right);
-
-	if (!tree->left && !tree->right)
-		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
 	if (left == right)
 		return (1);
 	else
 		return (0);
+
+}
+
+/**
+ * binary_tree_height - Calculates the height of a binary tree.
+ *
+ * Computes the height of the binary tree, which is the length of the
+ * longest path from the root node to a leaf node.
+ *
+ * @tree: Pointer to the root of the binary tree.
+ *
+ * Return: Height of the binary tree, or 0 if the tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_side, right_side;
+
+	if (tree == NULL)
+		return (0);
+
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	if (left_side > right_side)
+		return (left_side + 1);
+	else
+		return (right_side + 1);
 }
