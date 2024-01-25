627ce963ca55e684f06b456db72d46ec6b0cfd8b
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-01-25 07:25:28 -0800

    Code V2

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -24,7 +24,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left = binary_tree_is_perfect(tree->left);
 	right = binary_tree_is_perfect(tree->right);
 
-	if ((!tree->left && tree->right) || (tree->left && !tree->right))
+	if (!tree->left && !tree->right)
 		return (1);
 
 	if (left == right)
