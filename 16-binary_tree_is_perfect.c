8492cf4bf5e45a79be37ad2f1b95026adf474534
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-01-25 07:38:46 -0800

    V4 code, changes for return the conditional

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -24,7 +24,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	right = binary_tree_is_perfect(tree->right);
 
 	if (!tree->left && !tree->right)
-		return (1);
+		return (0);
 
 	if (left == right)
 		return (1);
